de968234b0887e700372e406bc656b02dd769fb5
Dreenyy <69611131+Dreenyy@users.noreply.github.com>
2020-08-12 18:16:09 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World.");
+		System.out.println("I'm Zain. I really don't know that much coding, but I have tried to. I know a little bit of Lua but it's only the basics.");
+		System.out.println("My hobbies include playing video games, learning how to script, watching shows, and just being on the computer.");
+		System.out.println("My favorite video games include league, valorant, osu!, and MW.");
+		System.out.println("I am not sure which college I plan to attend, but it will probably be SJSU.");
+		System.out.println("I am planning to become a software engineer and so I will be majoring CS, which is why I took this class. I hope it will be fun to code.");
+	}
+}
